0d0c2d900edc3e0ffabb794aea33c84f0f4704fd
Nguyen Thanh Tan <157698640+thanhtanvie@users.noreply.github.com>
2024-10-22 20:30:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
 var cors = require('cors')
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+require('dotenv').config();
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-    .then(()=> console.log('Connected'))
-    .catch(err => console.log(err));
+mongoose.set("strictQuery", false);
 
-var bodyParser = require('body-parser');
+MONGODB_URL= 'mongodb+srv://tan93493:<tan93493>@iot.8gxxw.mongodb.net/@iot?retryWrites=true&w=majority&appName=IoT';
+
+mongoose.connect(
+  MONGODB_URL,
+    () => {
+        console.log('Connected to MongoDB');
+    },
+);
 
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-app.use(cors())
-
+app.use(cors());
 const locationRoute = require('./api/routes/location.route');
 
 app.use("/v1/api/location",locationRoute);
+app.use("/",(req, res)=>{
+  res.json({data: "No Data"})
+});
 
-app.listen(process.env.PORT)
+
+app.listen(PORT, () => console.log(`server started ${PORT}`))
